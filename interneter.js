f726982c3b51f2d79b275540ff0bc4a582e0438a
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2023-11-16 11:04:55 -0500

    added interneter, for now

---
+++ interneter.js
@@ -0,0 +1,25 @@
+// Name: Interneter
+// ID: interneter
+// Description: Checks if the user is connected to the internet or not.
+// By: Wolfieboy09 <https://scratch.mit.edu/users/Wolfieboy09/>
+
+(function(Scratch) {
+  class Interneter {
+    getInfo() {
+      return {
+        id: 'interneter',
+        name: 'Interneter',
+        color1: '#ba4c14',
+        blocks: [
+          {
+            opcode: 'isConnected',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: 'connected to internet?',
+          }
+        ]
+      }
+    }
+    isConnected() { return navigator.onLine; }
+  }
+  Scratch.extensions.register(new Interneter())
+})(Scratch);
