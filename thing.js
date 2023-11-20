04928c82f4af3ac44fa0dfcb8509559cc998ef56
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2023-11-20 19:24:08 +0000

    thingy for someting

---
+++ thing.js
@@ -0,0 +1,26 @@
+(function(Scratch) {
+    
+    class SomeRandomThings {
+        getInfo() {
+            return {
+                id: 'somerandomness',
+                name: 'idk really',
+                blocks: [
+                    {
+                        opcode: 'copyToCLIPBOARD',
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: 'copy [TEXT] to clipboard',
+                        arguments: {
+                            TEXT: { type: Scratch.ArgumentType.STRING }
+                        }
+                    }
+                ]
+            };
+
+            copyToCLIPBOARD({ TEXT }) {
+                navigator.clipboard.writeText(TEXT)
+            }
+        }
+    }
+
+})(Scratch)
