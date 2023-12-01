38d67db5e1c9255521d940599367d7f78c4413d8
Andrew Beier <wolfieboy091yt@gmail.com>
2023-12-01 21:02:06 +0000

    a

--- custom/a.js
+++ custom/a.js
@@ -1,10 +1,37 @@
 (function(Scratch) {
-    
-    class GiveMeAname {
+    var JSON = {};
+
+    if (!Scratch.extensions.unsandboxed) {
+        throw new Error("Accounter must be unsandboxed");
+    }
+
+    class Accounter {
         getInfo() {
             return {
-                id: ''
+                id: 'accounter',
+                name: 'Accounter',
+                color1: '#27a376',
+                blocks: [
+                    {
+                        opcode: 'createAccount',
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: 'new account with username [USERNAME] with an id of [UID]',
+                        arguments: {
+                            USERNAME: { type: Scratch.ArgumentType.STRING, defaultValue: 'mistium' },
+                            NORUIDM_OR_DISC: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: "",
+                            }
+                        }
+                    }
+                ]
             }
         }
+
+        // functions go here
+        createAccount() {
+            
+        }
     }
+    Scratch.extensions.register(new Accounter())
 })(Scratch)
